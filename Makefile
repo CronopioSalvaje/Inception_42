a606ee3118e0f33edb08693709aef12efb622c87
CronopioSalvaje <chrisap@live.fr>
2024-10-03 19:54:51 +0200

    mariadb ok

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ COMPOSE = srcs/docker-compose.yml
 
 
 up:
-	docker compose -f $(COMPOSE) up
+	docker compose -f $(COMPOSE) up -d
 
 down:
 	docker compose -f $(COMPOSE) down
---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_USER', 'wordpress_user' );
+define( 'DB_PASSWORD', 'CeciEst1SuperMotDePasse!' );
+define( 'DB_HOST', 'localhost' );
+
+?>
