5c3bcce9ef7207d1c467716f29a1547656518f8e
CronopioSalvaje <chrisap@live.fr>
2024-10-13 23:04:17 +0200

    postix

--- srcs/requirements/bonus/postfix/tools/entrypoint.sh
+++ srcs/requirements/bonus/postfix/tools/entrypoint.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 set -e
 
+echo "Starting entrypoint script"
+
 # Vérification et correction des permissions
 for dir in defer incoming active deferred bounce corrupt hold; do
+    echo "Setting permissions for /var/spool/postfix/$dir"
     mkdir -p /var/spool/postfix/$dir
     chown postfix:postfix /var/spool/postfix/$dir
     chmod 700 /var/spool/postfix/$dir
 done
 
-chown -R postfix:postdrop /var/spool/postfix/public /var/spool/postfix/maildrop
+echo "Setting permissions for public and maildrop directories"
+chown -R postfix:postdrop /var/spool/postfix/public /var/spool/postfix/maildrop /var/spool/postfix/deferred/
 chmod 730 /var/spool/postfix/public /var/spool/postfix/maildrop
 
 # Assurez-vous que le fichier de log est accessible
+echo "Setting up mail log"
 touch /var/log/mail.log
 chmod 644 /var/log/mail.log
 
+echo "Running postfix check"
+postfix check
+
+echo "Starting Postfix"
 # Démarrage de Postfix
-exec "$@"
+
+exec postfix start-fg
+#exec "$@"
